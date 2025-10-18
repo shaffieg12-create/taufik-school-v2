ec360ad0ca75dc1e2e483c7e78eb3e4837a9250a
shaffieg12-create <shaffieg12@gmail.com>
2025-10-18 16:59:04 +0300

    Create seed.sql

---
+++ inc/inc/inc/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO classes(name,section,day_fee,boarding_fee,quran_fee) VALUES
+('Baby','nursery',350000,850000,0),
+('Middle','nursery',350000,850000,0),
+('Top','nursery',350000,850000,0),
+('P1','primary',450000,950000,50000),
+('P2','primary',450000,950000,50000),
+('P3','primary',480000,980000,50000),
+('P4','primary',480000,980000,50000),
+('P5','primary',500000,1000000,50000),
+('P6','primary',500000,1000000,50000),
+('P7','primary',520000,1020000,50000);
